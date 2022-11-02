872238dd1ae227e8158df603485ee9e414d8ec94
LipatoS <sasharubaiko@gmail.com>
2022-11-02 11:38:44 +0200

    Add static variables

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,25 @@
 package app;
 
 public class Main {
+    static int a;
+    static int b;
+    static int c;
+    static int d;
+    static int e;
+    static double f;
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
 
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int e = subtraction(a, b);
+        e = subtraction(a, b);
         System.out.println(e);
-        double f = divide(a, b);
+        f = divide(a, b);
         System.out.println(f);
     }
 
